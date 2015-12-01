afe30b2d180fd76e6c087530479e2f2f8ef49d8c
Dominic Scheirlinck <dominic@vendhq.com>
2015-12-01 13:57:45 +1300

    Test metric tags

--- test/Datadog/MetricTest.php
+++ test/Datadog/MetricTest.php
@@ -2,7 +2,6 @@
 
 namespace Vend\Statsd\Datadog;
 
-use Vend\Statsd\MetricInterface;
 use Vend\Statsd\MetricInterfaceTest;
 
 class MetricTest extends MetricInterfaceTest
@@ -11,10 +10,17 @@ class MetricTest extends MetricInterfaceTest
      * @param string $key
      * @param mixed  $value
      * @param string $type
-     * @return MetricInterface
+     * @return Metric
      */
     protected function getSut($key, $value, $type)
     {
         return new Metric($key, $value, $type);
+    }
+
+    public function testTags()
+    {
+        $metric = $this->getSut('foo', 1, 'c');
+        $metric->setTags(['something', 'host' => 'somewhere']);
+        $this->assertEquals('foo:1|c|#something,host:somewhere', $metric->getData());
     }
 }
